164a70908068021a5a9d2001f87513c528407b33
Fábio Adler <adlerluna@hotmail.com>
2026-04-16 14:03:26 -0300

    Versão 2.2

--- tools_windows.bat
+++ tools_windows.bat
@@ -3,6 +3,7 @@ chcp 65001 >nul
 cls
 title Tools Windows
 :main
+title Tools Windows
 cls
 echo  _____________________________________________________________________________________
 echo "                                                                                     "
@@ -24,7 +25,7 @@ echo "      \|____________|\|__|\|__| \|__|\|_______|\|_______|\|____________|\_
 echo "                                                                       \|_________|  "
 echo "                                                                                     "
 echo "     By: https://github.com/fabioadler   |  Name: Fábio Adler de L. Gomes            "
-echo "     Update: V0.2                                                                    "
+echo "     Update: V0.2.2                                                                  "
 echo "                                                                                     "
 echo "  1)  Backup drivers Windows                                                         "
 echo "  2)  Restaurar Backup Drivers Windows.                                              "
@@ -41,9 +42,11 @@ echo "  12) Ativar compressão de memoria.                                      
 echo "  13) Desativar compressão de memoria.                                               "
 echo "  14) Desativar controle inteligente de aplicativo.                                  "
 echo "  15) Ativar controle inteligente de aplicativo.                                     "
-echo "  16) Criar ponto de restauração.                                                    "
-echo "  17) Christitus ferramenta windows.                                                 "
-echo "  18) Winhance ferramenta windows.                                                   "
+echo "  16) Ver ponto de restauração.                                                      "
+echo "  17) Criar ponto de restauração.                                                    "
+echo "  18) Desabilitar limitação de criação de ponto de restauração.                      "
+echo "  19) Christitus ferramenta windows.                                                 "
+echo "  20) Winhance ferramenta windows.                                                   "
 echo "                                                                                     "
 echo "  0) Fechar.                                                                         "
 echo "                                                                                     "
@@ -164,17 +167,56 @@ if "%opcao%" == "0" (
     cls
     goto main
 ) else if "%opcao%" == "16" (
-    echo Iniciando...
-    set /p ponto=Digite o nome do ponto de recuperação:
-    Wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "%ponto%", 100, 7
+    echo Iniciando... Caso não mostre nada e por que não tem nenhum ponto criado
+    powershell -ExecutionPolicy Bypass -Command "Get-ComputerRestorePoint | Format-Table -AutoSize"
     pause
     goto main
-) if "%opcao%" == "17" (
+) else if "%opcao%" == "17" (
+    echo Iniciando...
+    set /p ponto=Digite o nome do ponto de restauração "sem espeaços":
+    echo  ________________________________________
+    echo " Escolha a opção de restauração a criar "
+    echo " 1) APPLICATION_INSTALL                 "
+    echo " 2) APPLICATION_UNINSTALL               "
+    echo " 3) CANCELLED_OPERATION                 "
+    echo " 4) DEVICE_DRIVER_INSTALL               "
+    echo " 5) MODIFY_SETTINGS                     "
+    echo "________________________________________"
+    set /p tprc=Digite o tipo de ponto de recuperação: 
+    if "%tprc%" == "1" (
+        powershell -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description %ponto% -RestorePointType APPLICATION_INSTALL"
+        pause
+    ) else if "%tprc%" == "2" (
+        powershell -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description %ponto% -RestorePointType APPLICATION_UNINSTALL"
+        pause
+    ) else if "%tprc%" == "3" (
+        powershell -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description %ponto% -RestorePointType CANCELLED_OPERATION"
+        pause
+    ) else if "%tprc%" == "4" (
+        powershell -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description %ponto% -RestorePointType DEVICE_DRIVER_INSTALL"
+        pause
+    ) else if "%tprc%" == "5" (
+        powershell -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description %ponto% -RestorePointType MODIFY_SETTINGS"
+        pause
+    ) else (
+        echo "Escolha uma opção valida! "
+        pause
+        cls
+        goto main
+    )
+    cls
+    goto main
+) else if "%opcao%" == "18" (
+    echo Iniciando...
+    REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /V "SystemRestorePointCreationFrequency" /T REG_DWORD /D 0 /F
+    cls
+    goto main
+) else if "%opcao%" == "19" (
     echo Iniciando...
     powershell -ExecutionPolicy Bypass -Command "irm christitus.com/win | iex"
     cls
     goto main
-) if "%opcao%" == "18" (
+) else if "%opcao%" == "20" (
     echo Iniciando...
     powershell -ExecutionPolicy Bypass -Command "irm https://get.winhance.net | iex"
     cls
